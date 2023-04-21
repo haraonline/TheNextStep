1b508127a89db1acf394f36f11e574d11959d763
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-21 18:08:30 +0200

    generics

--- 04_Generics/Program.cs
+++ 04_Generics/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //bool areEqual = AreEqual(true, false);
+            //bool areEqual = AreEqual(1, 1);
+            //bool areEqual = AreEqual('c', 'a');
+            bool areEqual = AreEqual("Harry", "Harry");
+
+            Console.WriteLine(areEqual);
+
+        }
+
+        public static bool AreEqual<T>(T first, T second) where T: IComparable<T>
+        {
+            return first.CompareTo(second) == 0;
         }
     }
 }
