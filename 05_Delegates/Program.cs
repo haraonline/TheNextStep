47b9824fbcba3f84a3d186c37f8ca686cfe47da4
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-23 17:56:53 +0200

    delegates

--- 05_Delegates/Program.cs
+++ 05_Delegates/Program.cs
@@ -1,5 +1,8 @@
 ﻿namespace _05_Delegates
 {
+
+    //Funk<> and Action<T> are generic delegates that can be used to represent methods that return void and take up to 16 parameters ???
+
     internal class Program
     {
         static void Main(string[] args)
